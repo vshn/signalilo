a733ef81e2cdfa54eba9ff25101443c928b0a6a2
Simon Gerber <simon.gerber@vshn.ch>
2019-02-18 10:38:00 +0100

    Add test for `checkOrCreateHost`

--- alertmanager_webhook_test.go
+++ alertmanager_webhook_test.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/Nexinto/go-icinga2-client/icinga2"
+	"github.com/corvus-ch/logr/buffered"
 )
 
 func isJSON(s string) bool {
@@ -41,5 +47,47 @@ func TestAsJSON(t *testing.T) {
 	if responseJSON.Message != "ok" {
 		t.Errorf("asJSON mangled response status: expected %v, got %v",
 			"ok", responseJSON.Message)
+	}
+}
+
+func checkLogMessage(t *testing.T, buf *bytes.Buffer, expected_msg string) {
+	actual_msg := strings.TrimSpace(buf.String())
+	if actual_msg != expected_msg {
+		t.Errorf("Log message: expected '%v', got '%v'", expected_msg, actual_msg)
+	}
+	// clear logger buffer
+	buf.Reset()
+}
+
+func TestCheckOrCreateHost(t *testing.T) {
+	icinga := icinga2.NewMockClient()
+	l := buffered.New(0)
+	hostname := "testhost1.local"
+
+	host, err := checkOrCreateHost(icinga, hostname, l)
+	if err != nil {
+		t.Errorf("Error creating host: %v", err)
+	}
+	checkLogMessage(t, l.Buf(), fmt.Sprintf("INFO creating host: %+v", hostname))
+	found := false
+	for _, h := range icinga.Hosts {
+		if h.Name == host.Name {
+			found = true
+		}
+	}
+	if !found {
+		t.Errorf("Unable to find host in icinga mock client")
+	}
+
+	// do checkOrCreate again -> should return same host struct and log
+	// "found host"
+	host2, err := checkOrCreateHost(icinga, "testhost1.local", l)
+	if err != nil {
+		t.Errorf("Error checking host: %v", err)
+	}
+	checkLogMessage(t, l.Buf(), fmt.Sprintf("INFO found host: %+v", hostname))
+	if host2.Name != host.Name {
+		t.Errorf("Created and found host name mismatch: expected %v, got %v\n",
+			host.Name, host2.Name)
 	}
 }
