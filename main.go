7e5f18571d0553063fec51cedc85cb85935c5254
Simon Gerber <simon.gerber@vshn.ch>
2019-07-01 14:50:08 +0200

    Bump in-application version to 0.1.0
    Relates: APPU-1751

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	app := kingpin.New("signalilo", "Signalilo takes in Alertmanager alerts through a webhook, translates them into Icinga2 services and posts them to Icinga using the Icinga API").Version("0.0.1")
+	app := kingpin.New("signalilo", "Signalilo takes in Alertmanager alerts through a webhook, translates them into Icinga2 services and posts them to Icinga using the Icinga API").Version("0.1.0")
 	configureServeCommand(app)
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
