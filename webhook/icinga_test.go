6e9bad6f07c20ae37647bd35fbe5c039690e7283
Simon Gerber <gesimu@gmail.com>
2020-03-16 11:44:33 +0100

    Refactor webhook icinga-related tests

--- webhook/icinga_test.go
+++ webhook/icinga_test.go
@@ -12,7 +12,6 @@ package webhook
 import (
 	"fmt"
 	"math"
-	"strconv"
 	"testing"
 
 	"git.vshn.net/appuio/signalilo/config"
@@ -21,135 +20,152 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateServiceName(t *testing.T) {
-	serviceNames := map[string]bool{
+type alertTestCase struct {
+	alert       template.Alert
+	nameCompute bool
+	heartbeat   bool
+	exitStatus  int
+	interval    float64
+	message     string
+}
+
+var (
+	regularAlerts = map[string]alertTestCase{
+		"basic service": {
+			template.Alert{
+				Labels: map[string]string{
+					"alertname": "service_up",
+					"service":   "testing",
+				},
+				Annotations: map[string]string{
+					"message": "the message 0",
+				},
+			}, true, false, 0, 0, "the message 0",
+		},
+	}
+	heartbeatAlerts = map[string]alertTestCase{
+		"heartbeat critical": {
+			template.Alert{
+				Status: "firing",
+				Labels: map[string]string{
+					"alertname": "heartbeat",
+					"service":   "testing",
+					"heartbeat": "60s",
+					"severity":  "critical",
+				},
+				Annotations: map[string]string{
+					"message": "the message 1",
+				},
+			}, true, true, 2, 66, "the message 1",
+		},
+		"heartbeat warning": {
+			template.Alert{
+				Status: "firing",
+				Labels: map[string]string{
+					"alertname": "heartbeat2",
+					"service":   "testing",
+					"heartbeat": "5m",
+					"severity":  "warning",
+				},
+				Annotations: map[string]string{
+					"message": "the message 2",
+				},
+			}, true, true, 1, 330, "the message 2",
+		},
+	}
+	serviceNames = map[string]struct {
+		name string
+		ok   bool
+	}{
 		// empty string should fail validation
-		"": false,
+		"empty string": {"", false},
 		// simple service names should pass validation
-		"a":       true,
-		"service": true,
-		"x":       true,
+		"simple name 1": {"a", true},
+		"simple name 2": {"service", true},
+		"simple name 3": {"x", true},
 		// no restrictions on leading special characters, should pass
 		// validation
-		"-b": true,
+		"leading special char": {"-b", true},
 		// @ is not allowed, should fail validation
-		"hello@example.com": false,
+		"no @": {"hello@example.com", false},
 		// the next name contains all the character classes that are
 		// allowed and should pass
-		"0+9:aA.bcZ,test_1": true,
+		"all allowed character classes": {"0+9:aA.bcZ,test_1", true},
 		// exactly 128 characters, should pass
-		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": true,
+		"128 chars": {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", true},
 		// 129 characters is longer than allowed, should fail
-		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab": false,
+		"129 chars": {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", false},
 	}
+)
 
-	for name, expected := range serviceNames {
-		assert.Equal(t, validateServiceName(name), expected, "service name regex works")
+func TestValidateServiceName(t *testing.T) {
+	for name, tcase := range serviceNames {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, validateServiceName(tcase.name), tcase.ok, "service name validation works correctly")
+		})
 	}
 }
 
 func TestComputeServiceName(t *testing.T) {
-	alerts := []template.Alert{
-		template.Alert{
-			Labels: map[string]string{
-				"alertname": "service_up",
-				"service":   "testing",
-			},
-			Annotations: map[string]string{
-				"expected": "true",
-			},
-		},
-	}
 
 	c := config.NewMockConfiguration(1)
 
-	for _, alert := range alerts {
-		svcName, err := computeServiceName(template.Data{}, alert, c)
-		expected, _ := strconv.ParseBool(alert.Annotations["expected"])
-		assert.Equal(t, err == nil, expected, fmt.Sprintf("Alert: %+v -> %v; err = %v", alert, svcName, err))
+	for name, tcase := range regularAlerts {
+		t.Run(name, func(t *testing.T) {
+			alert := tcase.alert
+			_, err := computeServiceName(template.Data{}, alert, c)
+			assert.Equal(t, err == nil, tcase.nameCompute, "service name computation successful")
+		})
+	}
+	for name, tcase := range heartbeatAlerts {
+		t.Run(name, func(t *testing.T) {
+			alert := tcase.alert
+			_, err := computeServiceName(template.Data{}, alert, c)
+			assert.Equal(t, err == nil, tcase.nameCompute, "service name computation successful")
+		})
 	}
 }
 
-func TestUpdateOrCreateService(t *testing.T) {
-	alerts := []template.Alert{
-		template.Alert{
-			Labels: map[string]string{
-				"alertname": "service_up",
-				"service":   "testing",
-			},
-			Annotations: map[string]string{
-				"heartbeat": "false",
-				"message":   "the message 0",
-			},
-		},
-		template.Alert{
-			Status: "firing",
-			Labels: map[string]string{
-				"alertname": "heartbeat",
-				"service":   "testing",
-				"heartbeat": "60s",
-				"severity":  "critical",
-			},
-			Annotations: map[string]string{
-				"heartbeat":  "true",
-				"exitStatus": "2",
-				"interval":   "66",
-				"message":    "the message 1",
-			},
-		},
-		template.Alert{
-			Status: "firing",
-			Labels: map[string]string{
-				"alertname": "heartbeat2",
-				"service":   "testing",
-				"heartbeat": "5m",
-				"severity":  "warning",
-			},
-			Annotations: map[string]string{
-				"heartbeat":  "true",
-				"exitStatus": "1",
-				"interval":   "330",
-				"message":    "the message 2",
-			},
-		},
-	}
+func TestUpdateOrCreateRegularService(t *testing.T) {
 	c := config.NewMockConfiguration(1)
 	i := icinga2.NewMockClient()
 
-	for idx, alert := range alerts {
-		svcName, err := computeServiceName(template.Data{}, alert, c)
-		displayName, err := computeDisplayName(template.Data{}, alert)
-		svc, err := updateOrCreateService(i, "test.vshn.net",
-			svcName, displayName, alert, c)
-		assert.Equal(t, err == nil, true, fmt.Sprintf("Alert: %+v -> %v; err = %v", alert, svc, err))
-		assert.Equal(t, svc.MaxCheckAttempts == 1, true, "soft states disabled for check %v", displayName)
-		isHeartbeat, _ := strconv.ParseBool(alert.Annotations["heartbeat"])
-		var state, check string
-		if isHeartbeat {
-			state = "enabled"
-			check = "heartbeat"
-		} else {
-			state = "disabled"
-			check = "regular"
-		}
-		assert.Equal(t, svc.EnableActiveChecks, isHeartbeat,
-			fmt.Sprintf("Active checking is %v on %v check %v", state, check, displayName))
-		if isHeartbeat {
-			checkInterval, _ := strconv.ParseFloat(alert.Annotations["interval"], 64)
-			assert.Equal(t, math.Abs(svc.CheckInterval-checkInterval) < 0.001,
-				isHeartbeat, "Check interval is correct on heartbeat check %v", displayName)
+	for name, tcase := range regularAlerts {
+		t.Run(name, func(t *testing.T) {
+			alert := tcase.alert
+			svcName, err := computeServiceName(template.Data{}, alert, c)
+			displayName, err := computeDisplayName(template.Data{}, alert)
+			svc, err := updateOrCreateService(i, "test.vshn.net", svcName, displayName, alert, c)
+			assert.Equal(t, err == nil, true, fmt.Sprintf("Alert: %+v -> %v; err = %v", alert, svc, err))
+			assert.Equal(t, svc.MaxCheckAttempts == 1, true, "soft states disabled for check %v", displayName)
+			assert.Equal(t, svc.EnableActiveChecks, false, "active checks disabled")
+			assert.Equal(t, svc.CheckInterval == 43200, true, "default check interval set")
+		})
+	}
+}
+
+func TestUpdateOrCreateHeartbeatService(t *testing.T) {
+	c := config.NewMockConfiguration(1)
+	i := icinga2.NewMockClient()
+
+	for name, tcase := range heartbeatAlerts {
+		t.Run(name, func(t *testing.T) {
+			alert := tcase.alert
+			svcName, err := computeServiceName(template.Data{}, alert, c)
+			displayName, err := computeDisplayName(template.Data{}, alert)
+			svc, err := updateOrCreateService(i, "test.vshn.net", svcName, displayName, alert, c)
+			assert.Equal(t, err == nil, true, "service creation successful")
+			assert.Equal(t, svc.MaxCheckAttempts == 1, true, "soft states disabled")
+			assert.Equal(t, svc.EnableActiveChecks, true, "active checks enabled")
+			// do math.Abs here to account for fp inaccuracies
+			// when testing for equality
+			assert.Equal(t, math.Abs(svc.CheckInterval-tcase.interval) < 0.0001, true, "check interval correct")
 			dummyText, ok := svc.Vars["dummy_text"]
-			assert.Equal(t, ok, true, "Dummy text is set on heartbeat check %v", displayName)
 			dummyState, ok := svc.Vars["dummy_state"]
-			assert.Equal(t, ok, true, "Dummy state is set on heartbeat check %v", displayName)
-			exitStatus, _ := strconv.Atoi(alert.Annotations["exitStatus"])
-			assert.Equal(t, dummyState == exitStatus, true,
-				"dummy_state of heartbeat is configured correctly for %v", displayName)
-			msg := fmt.Sprintf("the message %d", idx)
-			assert.Equal(t, dummyText == msg, true,
-				"dummy_text of heartbeat is configured correctly for %v", displayName)
-		} else {
-			assert.Equal(t, svc.CheckInterval == 43200, true)
-		}
+			assert.Equal(t, ok, true, "Dummy text is set")
+			assert.Equal(t, ok, true, "Dummy state is set")
+			assert.Equal(t, dummyState == tcase.exitStatus, true, "dummy_state of heartbeat is correct")
+			assert.Equal(t, dummyText == tcase.message, true, "dummy_text of heartbeat is correct")
+		})
 	}
 }
